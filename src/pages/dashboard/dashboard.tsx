d57ebb0b5829c2d7d0f12e4a185e9d39acf8715b
wakhiwemathuthu <wakhiwemathuthu6@gmail.com>
2024-04-21 18:46:52 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/index.css 	modified:   src/pages/dashboard/dashboard.tsx

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,4 +1,6 @@
 import Header from "../../components/header/header";
+import { Calendar } from "react-calendar";
+// import "react-calendar/dist/Calendar.css";
 
 function Dashboard() {
   return (
@@ -6,7 +8,13 @@ function Dashboard() {
       <Header title="Dashboard" />
       <div className="absolute top-20 left-0 right-0 bottom-0 grid grid-cols-4 gap-4 p-4">
         <div className="border-2 border-green-400 col-span-3">welcome</div>
-        <div className="border-2 border-green-400 row-span-2">calender</div>
+        <div className="row-span-2 p-4 rounded-2xl bg-white shadow-2xl">
+          <Calendar
+            onChange={() => {}}
+            value={[new Date(), new Date("25-04-23")]}
+            className="text-primary"
+          />
+        </div>
         <div className="border-2 border-green-400">progress</div>
         <div className="border-2 border-green-400 col-span-2">activity</div>
         <div className="border-2 border-green-400 col-span-3">Courses</div>
