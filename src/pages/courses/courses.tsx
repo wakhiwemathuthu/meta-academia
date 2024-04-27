e04d410d5c27a980ef9835657dc71d0b822aafaa
wakhiwemathuthu <wakhiwemathuthu6@gmail.com>
2024-04-27 09:47:37 +0200

    modified:   src/pages/courses/courses.tsx

--- src/pages/courses/courses.tsx
+++ src/pages/courses/courses.tsx
@@ -52,12 +52,10 @@ function Courses() {
                     </button>
                   </div>
                 </div>
-
                 <p className="text-primary opacity-60 mt-1">
                   Learn how to apply User Expereience (UX) principles to your
                   website designs
                 </p>
-
                 <div className="flex items-center gap-2 text-primary mt-2">
                   <div className="flex items-center gap-2">
                     <img
@@ -124,7 +122,103 @@ function Courses() {
             </div>
           </div>
         </div>
-        <div className="rounded-2xl shadow-2xl bg-white lg:row-span-3"></div>
+        <div className="rounded-2xl shadow-2xl bg-white lg:row-span-3 p-4">
+          <div className="flex items-center mb-4 justify-between">
+            <h2 className="font-bold text-primary">Recomended Courses</h2>
+          </div>
+          <div className="hover:shadow-4xl hover:border-white border border-neutral-100 transition-all cursor-pointer rounded-2xl p-4">
+            <img
+              src="https://img.freepik.com/free-photo/view-3d-laptop-device-with-screen-keyboard_23-2150714005.jpg?t=st=1714160808~exp=1714164408~hmac=d31b6ae9d0e4198b4bbfe304cd0648ee7ea9146ebf855901eba0b5ef6a76f50f&w=826"
+              alt=""
+              className="block h-20 w-20 object-cover rounded-xl float-left mr-3"
+            />
+            <div className="w-full">
+              <div className="flex items-center justify-between">
+                <p className="font-semibold text-sm text-primary">
+                  UX & Web Design Master Course: Strategy, Design, Development
+                </p>
+                <div className="">
+                  <button
+                    onClick={toggleLike}
+                    className="btn btn-ghost btn-circle hover:bg-neutral-100"
+                  >
+                    {liked ? (
+                      <IoMdHeart className="text-xl text-red-500" />
+                    ) : (
+                      <IoMdHeartEmpty className="text-xl text-red-500" />
+                    )}
+                  </button>
+                </div>
+              </div>
+              <p className="text-primary opacity-60 mt-1 text-sm">
+                Learn how to apply User Expereience (UX) principles to your
+                website designs
+              </p>
+              <div className="flex items-center gap-2 text-primary mt-2 text-sm">
+                <div className="flex items-center gap-2">
+                  <img
+                    src="https://img.freepik.com/free-psd/3d-illustration-human-avatar-profile_23-2150671142.jpg?t=st=1714161128~exp=1714164728~hmac=592a4ff886d452e0cb54cbe8f6f5701e95acc4fac966ae723b112b0a0b4ddf19&w=826"
+                    alt=""
+                    className="block w-6 h-6 rounded-full"
+                  />
+                  <p className="opacity-60">Wakhiwe Mathuthu</p>
+                </div>
+                <BsDot className="opacity-60" />
+                <div className="flex items-center gap-2 opacity-60">
+                  <PiFilesBold />
+                  <p>62 lectures</p>
+                </div>
+                <BsDot className="opacity-60" />
+                <div className="flex items-center gap-2 opacity-60">
+                  <LuClock3 />
+                  <p>3h 4m total length</p>
+                </div>
+              </div>
+              <div className="flex items-center justify-between mt-3">
+                <div className="flex items-center gap-2">
+                  <span className=" badge text-primary">Web Designing</span>
+                  <span className=" badge">UI /UX Designing</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <p className="text-sm text-primary">(3/5)</p>
+                  <div className="rating rating-sm">
+                    <input
+                      disabled
+                      type="radio"
+                      name="rating-2"
+                      className="mask mask-star-2 bg-orange-400"
+                    />
+                    <input
+                      disabled
+                      type="radio"
+                      name="rating-2"
+                      className="mask mask-star-2 bg-orange-400"
+                    />
+                    <input
+                      disabled
+                      checked
+                      type="radio"
+                      name="rating-2"
+                      className="mask mask-star-2 bg-orange-400"
+                    />
+                    <input
+                      disabled
+                      type="radio"
+                      name="rating-2"
+                      className="mask mask-star-2 bg-orange-400"
+                    />
+                    <input
+                      disabled
+                      type="radio"
+                      name="rating-2"
+                      className="mask mask-star-2 bg-orange-400"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
