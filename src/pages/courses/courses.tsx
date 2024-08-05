dd4fd42347b9bd529170fba47c203b6000caa631
wakhiwemathuthu <wakhiwemathuthu@gmail.com>
2024-08-05 11:46:13 +0200

    updated

--- src/pages/courses/courses.tsx
+++ src/pages/courses/courses.tsx
@@ -1,12 +1,6 @@
 import Header from "../../components/header/header";
-import { useState } from "react";
 
 function Courses() {
-  const [liked, setLiked] = useState(false);
-
-  const toggleLike = () => {
-    setLiked(!liked);
-  };
 
   return (
     <div className="h-full">
